42499ff7d7762810d7ab4d5f89b0ed50b59572bf
Goran Mladenovic <59745165+LastCaballero@users.noreply.github.com>
2022-09-14 11:31:38 +0200

    Update bitset_magic.cpp

--- Shorts/bitset_magic.cpp
+++ Shorts/bitset_magic.cpp
@@ -9,9 +9,8 @@ int main () {
         string oct_2 =  bitset<8>(200).to_string() ;
         string oct_3 =  bitset<8>(200).to_string() ;
         string oct_4 =  bitset<8>(200).to_string() ;
-  
-        cout << bitset<32>( oct_1
-                            + oct_2
-                            + oct_3
-                            + oct_4 ).to_string() << endl ;
+
+        bitset<32> ip( oct_1 + oct_2 + oct_3 + oct_4 ) ;
+        
+        cout << ip.to_string() << endl ;
 }
